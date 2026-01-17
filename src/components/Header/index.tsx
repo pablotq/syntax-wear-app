e7c0c59d1d9140e89414294a1d13298afa15cdc4
pablotq <pablo.cstds@gmail.com>
2026-01-17 09:58:37 -0300

    rresponsivo do Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,10 +8,11 @@ export const Header = () => {
     <div className="relative">
       <header className="fixed bg-amber-700 top-0 left-0 right-0 z-10 mx-10">
         <div className='bg-white max-w-330 mx-auto flex justify-between items-center py-5 px-7 rounded-2xl mt-5'>
-          <img src={Logo} alt="Logo SyntaxWear" />
+          <img src={Logo} alt="Logo SyntaxWear" 
+          className='w-32 md:w-36'/>
 
-          <nav>
-            <ul className='flex gap-10'>
+          <nav className='hidden md:block'>
+            <ul className='flex gap-4 lg:gap-10'>
               <li><a href="#">Masculino</a></li>
               <li><a href="#">Feminino</a></li>
               <li><a href="#">Outlet</a></li>
@@ -19,9 +20,9 @@ export const Header = () => {
           </nav>
 
           <nav>
-            <ul className='flex gap-10'>
-              <li><a href="#">Nossas lojas</a></li>
-              <li><a href="#">Sobre</a></li>
+            <ul className='flex gap-4 lg:gap-10'>
+              <li className='hidden md:block'><a href="#">Nossas lojas</a></li>
+              <li className='hidden md:block'><a href="#">Sobre</a></li>
               <li><a href="#"><img src={IconUser} alt="Ícone de login" /></a></li>
               <li><a href="#"><img src={IconAbout} alt="Ícone sobre" /></a></li>
               <li><a href="#"><img src={IconsCart} alt="Ícone carrinho" /></a></li>

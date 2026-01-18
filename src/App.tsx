2fdc9e5f97a4c544163279e662b33f8b935764b5
pablotq <pablo.cstds@gmail.com>
2026-01-18 10:23:54 -0300

    adi;cão do componente Hero e de seu banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Header } from "./components/Header"
+import { Hero } from "./components/Hero"
 
 
 function App() {
   return (
     <>
       <Header />
+      <main className="py-10">
+        <Hero />
+      </main>
     </>
   )
 }

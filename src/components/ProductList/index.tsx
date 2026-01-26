86a78fcfdf9844d7eef0f88208e143995bb37822
pablotq <pablo.cstds@gmail.com>
2026-01-26 16:52:44 -0300

    feat: listagem de produtos

---
+++ src/components/ProductList/index.tsx
@@ -0,0 +1,16 @@
+import type { Product } from "../../interfaces/product"
+import { ProductCard } from "../ProductCard";
+
+interface ProductCardProps {
+    products: Product[]
+}
+
+export const ProductList = ({products}: ProductCardProps) =>{
+    return(
+        <section className="grid gap-4 grid-cols-[repeat(auto-fit,minmax(350px,1fr))] mb-10">
+            {products.map(product => (
+                <ProductCard product={product}/>
+            ))}
+        </section>
+    );
+}
--- src/pages/_app/products/index.tsx
+++ src/pages/_app/products/index.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { ProductList } from '../../../components/ProductList';
+import { products } from '../../../mocks/products';
 
 export const Route = createFileRoute('/_app/products/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_app/products/"!</div>
+  return (
+    <div className='container pt-40 pb-10 md:px-10 mb-10 text-black bg-surface'>
+      <h1 className="text-3xl text-center mb-3">Lista de Produtos</h1>
+
+      <h2 className='text-center mb-10 p-2'>Conforto excepcional para suas aventuras do dia-a-dia</h2>
+      <ProductList products={products} />
+    </div>
+  );
 }
